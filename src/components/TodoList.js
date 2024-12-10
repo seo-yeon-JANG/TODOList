f11b0e997710c622977746e177afac393632fc6a
장서연 SKTelecom <tjsus247@naver.com>
2024-12-10 11:11:12 +0900

    화면단 스타일링 적용

---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+import TodoListItem from './TodoListItem';
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
+export default TodoList;
